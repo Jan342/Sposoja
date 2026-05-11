3df0e625222839a66b9d610db5007e50f6e792d9
UrosVucetic <uros.vucetic@student.um.si>
2026-05-11 15:40:11 +0200

    Implemented dependencies

--- Programski_jeziki/app/build.gradle.kts
+++ Programski_jeziki/app/build.gradle.kts
@@ -56,4 +56,10 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.retrofit2:converter-gson:3.0.0")
+    implementation("com.google.mlkit:barcode-scanning:17.3.0")
+    implementation("androidx.camera:camera-camera2:1.3.0")
+    implementation("androidx.camera:camera-lifecycle:1.3.0")
+    implementation("androidx.camera:camera-view:1.3.0")
 }
