96aba4e5e925f897b7b2fd9327fb0cf9e35ef02a
UrosVucetic <uros.vucetic@student.um.si>
2026-05-18 16:47:57 +0200

    Logotype
    Added homepage logotype.

--- Razvoj_aplikacij_za_internet/frontend/src/components/Header.tsx
+++ Razvoj_aplikacij_za_internet/frontend/src/components/Header.tsx
@@ -1,12 +1,19 @@
 import { Link } from "react-router-dom";
 import { UserContext } from "../contexts/userContext";
 import { Navbar, Nav } from "react-bootstrap";
-
+import logo from "../assets/logo.svg";
 
 function Header() {
   return (
     <Navbar bg="light" expand="lg">
       <Navbar.Brand as={Link} to="/">
+        <img
+          src={logo}
+          width="45"
+          height="45"
+          className="d-inline-block align-top me-2 ms-4"
+          alt="Logo"
+        />{" "}
         Domov
       </Navbar.Brand>
 
