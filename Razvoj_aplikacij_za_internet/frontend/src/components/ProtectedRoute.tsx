306c8bba8eef5515be58cba3a3853d2ce46a3b2d
dejkoz <dejkoz123@gmail.com>
2026-05-19 22:36:15 +0200

    added context for club

--- Razvoj_aplikacij_za_internet/frontend/src/components/ProtectedRoute.tsx
+++ Razvoj_aplikacij_za_internet/frontend/src/components/ProtectedRoute.tsx
@@ -4,15 +4,21 @@ import { Navigate } from "react-router-dom";
 
 type ProtectedRouteProps = {
   children: JSX.Element;
+  role: "user" | "club";
 };
 
 function ProtectedRoute({
   children,
+  role
 }: ProtectedRouteProps) {
   const userObj = useContext(UserContext);
 
   if (!userObj.user) {
     return <Navigate to="/login" replace />;
+  }
+
+  if (role && userObj.user.accountType !== role) {
+    return <Navigate to="/dashboard" replace />;
   }
 
   return children;
