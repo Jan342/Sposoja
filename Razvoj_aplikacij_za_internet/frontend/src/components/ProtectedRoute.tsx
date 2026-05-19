19642f485bd990eadc27103daa1180f636f316f7
dejkoz <dejkoz123@gmail.com>
2026-05-19 22:47:19 +0200

    fixed role attribute for ProtectedRoute

--- Razvoj_aplikacij_za_internet/frontend/src/components/ProtectedRoute.tsx
+++ Razvoj_aplikacij_za_internet/frontend/src/components/ProtectedRoute.tsx
@@ -4,7 +4,7 @@ import { Navigate } from "react-router-dom";
 
 type ProtectedRouteProps = {
   children: JSX.Element;
-  role: "user" | "club";
+  role?: "user" | "club";
 };
 
 function ProtectedRoute({
